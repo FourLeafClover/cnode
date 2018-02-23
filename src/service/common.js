5e7a61d3106caa39c96d2b82ab3f3ee107d7bec7
frank.zhang <zc_smile@outlook.com>
2018-02-23 11:23:17 +0800

    修改终端判断的代码

--- src/service/common.js
+++ src/service/common.js
@@ -23,19 +23,9 @@ let formatDate = (dateStr, format) => {
 }
 
 let getClientType = () => {
-    var sUserAgent = navigator.userAgent.toLowerCase();
-    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-    var bIsAndroid = sUserAgent.match(/android/i) == "android";
-    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile"
-
     if (navigator.userAgent.match(/(iPhone|iPod|iPad);?/i)) {
         return "IOS";
-    } else if (navigator.userAgent.match(/android/i) == "android") {
+    } else if (navigator.userAgent.toLowerCase().match(/android/i) == "android") {
         return "Android";
     } else {
         return "PC";
